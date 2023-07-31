3c08d0a7ddd3d703e6613e25d8036d96980fa171
LINavy <hatake18@bk.ru>
2023-08-01 00:55:31 +0300

    hw module2-task1

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import Main from '../../pages/main/main';
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppScreenProps): JSX.Element {
+  return (
+    <Main offersCount={offersCount} />
+  );
+}
+
+export default App;
