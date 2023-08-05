db9f7d928948dc8e48b4e91e6a2e6df0769d6867
Che5ter <46726546+Che5ter@users.noreply.github.com>
2023-08-05 15:58:08 +0300

    hw module3-task1 (#3)

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,16 @@
 export const Setting = {
-  offersCount: 5
+	offersCount: 5
 };
+
+export enum AppRoute {
+	Root = '/',
+	Login = '/login',
+	Favorites = '/favorites',
+	Offer = '/offer'
+}
+
+export enum AuthorizationStatus {
+	Auth = 'AUTH',
+	NoAuth = 'NO_AUTH',
+	Uknown = 'UKNOWN'
+}
